d5e5eda0f5398aa2ebeabb3f79b4a10dc6810a4c
Mrlumutz <adlan.afif.nugroho@gmail.com>
2023-05-20 12:37:58 +0700

    updated

--- src/component/elements/Loader/index.tsx
+++ src/component/elements/Loader/index.tsx
@@ -1,6 +1,6 @@
 import { Loading } from '@nextui-org/react';
 import { classicNameResolver } from 'typescript';
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
 
 
 function Loader() {
